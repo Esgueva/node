ce219075dd6d3cf688a88696aa7c5c1dfdd7dd9c
Esgueva <esgueva@gmail.com>
2020-05-19 14:14:28 +0200

    update/config

--- movies-api/config/index.js
+++ movies-api/config/index.js
@@ -7,7 +7,13 @@ const config = {
   dbUser: process.env.DB_USER,
   dbPass: process.env.DB_PASSWORD,
   dbName: process.env.DB_NAME,
-  dbHost: process.env.DB_HOST
+  dbHost: process.env.DB_HOST,
+  authJwtSecret: process.AUTH_JWT_SECRET,
+  defaultAdminPassword: process.env.DEFAULT_ADMIN_PASSWORD,
+  defaultUserPassword: process.env.DEFAULT_USER_PASSWORD,
+  authJwtSecret: process.env.AUTH_JWT_SECRET,
+  adminApiKeyToken: process.env.ADMIN_API_KEY_TOKEN,
+  publicApiKeyToken: process.env.PUBLIC_API_KEY_TOKEN
 };
 
 module.exports = { config };
